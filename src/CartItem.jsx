64a1aadfad577e06b2131670ff638ebef7ce5467
maahedahss <maahedahsadiq@gmail.com>
2025-07-11 14:22:37 -0400

    First update

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,20 +9,28 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    var total = 0; 
+    cart.forEach(total += item.quantity * parseFloat(item.cost.substring(1)));
+    return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatchEvent(updateQuantity(item.quantity + 1));
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 1) {
+        dispatch(updateQuantity(item.quantity - 1));
+    } else {
+        dispatch(removeItem(item));
+    }
   };
 
   const handleRemove = (item) => {
@@ -30,6 +38,10 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+  };
+
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
   };
 
   return (
