764003f5dca7562dbdab02f08dce60ee0635719f
maahedahss <maahedahsadiq@gmail.com>
2025-07-12 11:27:11 -0400

    Fixed number of items in cart

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -4,6 +4,7 @@ export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
     items: [], // Initialize items as an empty array
+    itemsInCart: 0 // Number of items currently in cart
   },
   reducers: {
     addItem: (state, action) => {
@@ -15,15 +16,23 @@ export const CartSlice = createSlice({
         } else {
             state.items.push({name, image, cost, quantity: 1});
         }
+        
+        state.itemsInCart++;
     },
     removeItem: (state, action) => {
         const {name, quantity} = action.payload;
         state.items = state.items.filter(item => item.name !== name);
+        state.itemsInCart -= quantity;
     },
     updateQuantity: (state, action) => {
         const {name, quantity} = action.payload;
         const existingItem = state.items.find(item => item.name === name);
         if (existingItem) {
+            if (quantity == existingItem.quantity + 1) {
+                state.itemsInCart++;
+            } else {
+                state.itemsInCart--;
+            }
             existingItem.quantity = quantity;
         }
     }
